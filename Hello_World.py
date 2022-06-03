0fe27eb6ac0f28443e3b306ef49337017ddfdeba
Andrew Karnaushenko <allloneneon@gmail.com>
2022-06-03 17:04:47 +0300

    commit1

--- Hello_World.py
+++ Hello_World.py
@@ -1,15 +1,1 @@
-count_people = 5
-
-years_jack = 20
-
-years_hanna = 23
-
-years_bobby = 15
-
-years_john = 16
-
-years_drake = 35
-
-avarege_years = (years_jack + years_hanna + years_bobby + years_john +years_drake)/count_people
-
-print(avarege_years)
+print('hello')
